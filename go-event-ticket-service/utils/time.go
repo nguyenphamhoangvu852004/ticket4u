cd64c12142120a4f22bd3eb572a65391f7646b15
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-01-13 11:16:26 +0700

    Add: utils for time

--- go-event-ticket-service/utils/time.go
+++ go-event-ticket-service/utils/time.go
@@ -1,2 +1,15 @@
 package utils
 
+import (
+	"fmt"
+	"time"
+)
+
+func IsValidTimeFormatISO8601(dateString string) bool {
+	_, err := time.Parse("2006-01-02", dateString)
+	fmt.Println(err)
+	if err != nil {
+		return false
+	}
+	return true
+}

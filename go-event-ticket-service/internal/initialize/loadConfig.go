6e34f83b6fa8e1443924613e5cc1a9225297b51d
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-01-13 11:21:24 +0700

    Mod: change logic detach env file

--- go-event-ticket-service/internal/initialize/loadConfig.go
+++ go-event-ticket-service/internal/initialize/loadConfig.go
@@ -3,25 +3,35 @@ package initialize
 import (
 	"fmt"
 	"go-event-ticket-service/global"
+	"os"
 
 	"github.com/spf13/viper"
 )
 
 func LoadConfig() {
 
-	viper := viper.New()
-	viper.AddConfigPath("../config")
-	viper.SetConfigName("dev")
-	viper.SetConfigType("yaml")
+	viper := viper.New() // *viper.Viper
+	env := os.Getenv("APP_ENV")
+
+	switch env {
+	case "docker":
+
+		viper.AddConfigPath("../config")
+		viper.SetConfigName("config.docker")
+		viper.SetConfigType("yaml")
+	case "local":
+
+		viper.AddConfigPath("./config")
+		viper.SetConfigName("config.local")
+		viper.SetConfigType("yaml")
+	default:
+		panic("Wrong environment")
+	}
 
 	// // đọc config
 	if err := viper.ReadInConfig(); err != nil {
 		panic(fmt.Errorf("Failed to read in YAML file: %w", err))
 	}
-
-	fmt.Printf("Raw config: %#v\n", viper.AllSettings())
-	// fmt.Println("Server Port: ", viper.GetInt("server.port"))
-	// fmt.Println("Database Name: ", viper.GetString("mysql.host"))
 
 	if err := viper.Unmarshal(&global.Config); err != nil {
 		panic(fmt.Errorf("Failed to unmarshal YAML file: %w", err))

07767d023d8e6e63d23e1f8c7e6ca1d61eaccf26
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-01-21 10:47:14 +0700

    config rate limiting apigateway

--- api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -1,15 +1,39 @@
 package com.example.api_gateway;
 
+import java.util.Objects;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
+	private final ReactiveStringRedisTemplate redisTemplate;
+
+	public ApiGatewayApplication(ReactiveStringRedisTemplate redisTemplate) {
+		this.redisTemplate = redisTemplate;
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public KeyResolver ipKeyResolver() {
+		return exchange -> {
+			// Log ra để biết chắc chắn nó được gọi
+			System.out.println(">>> RATE LIMITER ĐANG CHẶN IP: " +
+					exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+
+			return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+		};
+	}
 }
